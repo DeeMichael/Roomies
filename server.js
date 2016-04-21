a0f96ab43720a3b195f1631051dd20e8903046f6
DeeMichael <deemichaelb@gmail.com>
2016-04-21 11:10:38 -0600

    finished inital setup of server.js

--- server.js
+++ server.js
@@ -14,3 +14,18 @@ mongoose.connect(dbURL,function(err){
   if (err) console.log("!-- Failed to connect to roomies db.")
   else console.log("-- Connected to roomies db.")
 })
+
+//  Standard Middleware
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+//  Configure Routing
+app.use(express.static(__dirname + '/public'))
+app.use('/user-api/v1', userRoutes)
+
+//  Set up Listener
+app.listen(port, function(err){
+  if (err) console.log("!-- Server Not Running: ", err)
+  else console.log("-- Server Running on Port: ", port)
+})
